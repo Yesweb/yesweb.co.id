0fb408461f2476b3d1f869d706000d34a93938eb
Wiyoso Aji <aji_gondronk@yahoo.com>
2013-06-13 09:40:33 +0700

    update script slide show

---
+++ system/cms/logs/log-2013-06-13.php
@@ -0,0 +1,4 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2013-06-13 02:30:20 --> Severity: Runtime Notice  --> is_a(): Deprecated. Please use the instanceof operator C:\AppServ\www\yesweb\yesweb.co.id\system\cms\core\MY_Controller.php 78
+ERROR - 2013-06-13 02:38:36 --> Severity: Runtime Notice  --> is_a(): Deprecated. Please use the instanceof operator C:\AppServ\www\yesweb\yesweb.co.id\system\cms\core\MY_Controller.php 78
