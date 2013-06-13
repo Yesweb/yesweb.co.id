2215605f458f82e6e977101075119763531fbeda
Wiyoso Aji <aji_gondronk@yahoo.com>
2013-06-13 22:10:28 +0700

    fixing css
    perbaikan css untuk menampilkan news

--- system/cms/logs/log-2013-06-13.php
+++ system/cms/logs/log-2013-06-13.php
@@ -2,3 +2,5 @@
 
 ERROR - 2013-06-13 02:30:20 --> Severity: Runtime Notice  --> is_a(): Deprecated. Please use the instanceof operator C:\AppServ\www\yesweb\yesweb.co.id\system\cms\core\MY_Controller.php 78
 ERROR - 2013-06-13 02:38:36 --> Severity: Runtime Notice  --> is_a(): Deprecated. Please use the instanceof operator C:\AppServ\www\yesweb\yesweb.co.id\system\cms\core\MY_Controller.php 78
+ERROR - 2013-06-13 13:08:40 --> Severity: Runtime Notice  --> is_a(): Deprecated. Please use the instanceof operator C:\AppServ\www\yesweb\yesweb.co.id\system\cms\core\MY_Controller.php 78
+ERROR - 2013-06-13 15:04:51 --> Severity: Runtime Notice  --> is_a(): Deprecated. Please use the instanceof operator C:\AppServ\www\yesweb\yesweb.co.id\system\cms\core\MY_Controller.php 78
